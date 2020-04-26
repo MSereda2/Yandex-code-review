90925b36d935a92f70f7c7462c807f3be6561f4f
Misha Sereda <sereda2510@mail.ru>
2020-04-26 17:11:55 +0300

    re-comit

--- javascript, react/Academy-v.1.5.0-javascript/Academy-v.1.5.0-javascript/utils/enums.js
+++
@@ -1,6 +0,0 @@
-const PageEnum = { // bad name of const
-  SiteWrapper: {
-    SEARCH: document.querySelector(`.columns__search-item`),
-    rating: document.querySelector(`.columns__rating`)
-  }
-};
--- javascript, react/Academy-v1.0.0-react/Academy-v1.0.0-react/src/components/welcome-screen/welcome-screen.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const WelcomeScreen = () => {
-  return (
-      <h2>Главная страница</h2>
-  )
-}
-
-export default WelcomeScreen
